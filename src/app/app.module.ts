9a70b7f89e7a9d9cfea614c8bc7d01af9bc94e1d
lvasilev <lyubomir.vasilev@adesso.bg>
2022-10-30 18:11:51 +0100

    Created basic components with basic nav structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ShoppingList } from './header/shopping-list/component/shopping-list/shopping-list.component';
+import { Header } from './header/component/header.component';
+import { RecipeListComponent } from './header/recipes/component/recipe-list/recipe-list.component';
+import { RecipeComponent } from './header/recipes/component/recipe/recipe.component';
+import { RecipeDetailsComponent } from './header/recipes/component/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './header/recipes/component/recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header,
+    ShoppingList,
+    RecipeListComponent,
+    RecipeComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
