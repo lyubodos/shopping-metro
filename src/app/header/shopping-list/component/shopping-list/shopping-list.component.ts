7fba8fd6755e39c841315e05cd793bab9dcb74fb
lvasilev <lyubomir.vasilev@adesso.bg>
2022-10-31 09:25:25 +0100

    Shopping list and recipe services ready
    Implemented basic functionality of rthe shopping list service and the
    recipe service.
    src/app/header/shopping-list/services/shopping-list.service.ts#

--- src/app/header/shopping-list/component/shopping-list/shopping-list.component.ts
+++ src/app/header/shopping-list/component/shopping-list/shopping-list.component.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit } from "@angular/core";
 import { Ingredients } from "../../data/ingredients.model";
+import { shoppingListService } from "../../services/shopping-list.service";
 
 
 @Component({
     selector: "app-shopping-list",
     templateUrl: "./shopping-list.component.html",
-    styleUrls: ["./shopping-list.component.css"]
+    styleUrls: ["./shopping-list.component.css"],
+    providers: [shoppingListService]
 })
 export class ShoppingList implements OnInit {
-    ingredients: Ingredients[] = [
-        {
-            name: "Apples",
-            amount: 5
-        },
-        {
-            name: "Tomaatoes",
-            amount: 11
-        }
-    ]
+    ingredients: Ingredients[];
+    selectedIngrediens: Ingredients;
 
-    constructor(){}
+    constructor(private shoppingListService: shoppingListService){}
 
     ngOnInit(): void {
+        this.ingredients = this.shoppingListService.getIngredients();
+    }
+
+    public loadIngredients(index: number){
+        this.selectedIngrediens = this.shoppingListService.getIngredient(index);
+        console.log(this.selectedIngrediens);
         
     }
 }
--- src/app/header/shopping-list/services/shopping-list.service.ts
+++ src/app/header/shopping-list/services/shopping-list.service.ts
@@ -1,3 +1,25 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredients } from "../data/ingredients.model";
+
 export class shoppingListService {
+   private ingredients: Ingredients[] = [
+        {
+            name: "Apples",
+            amount: 5
+        },
+        {
+            name: "Tomatoes",
+            amount: 11
+        }
+    ];
     
+    public ingredientSelected = new EventEmitter<Ingredients>();
+
+    public getIngredients(): Ingredients[] {
+        return this.ingredients.slice();
+    }
+
+    public getIngredient(index: number): Ingredients{
+        return this.ingredients[index];
+    }
 }
