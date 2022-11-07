5072d90b5ba7769e4003aa1900bf7c0a1145e387
lvasilev <lyubomir.vasilev@adesso.bg>
2022-11-07 18:58:21 +0100

    Added starting functionality for validation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-auth',
@@ -7,5 +8,16 @@ import { Component } from "@angular/core";
 })
 
 export class AuthComponent {
-        
+ @ViewChild('authForm') authForm: NgForm;
+
+ public isAuthenticated: boolean = false;
+
+ public onSwitchMode(): void {
+    this.isAuthenticated = !this.isAuthenticated;
+    this.authForm.reset();
+ }
+
+ public onSubmit(form: NgForm): void {
+    console.log(form);
+ }
 }
