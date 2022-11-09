f77e309c4c9c38f5541606912d052aba0933932e
lvasilev <lyubomir.vasilev@adesso.bg>
2022-11-09 17:40:49 +0100

    Added a new user model for the loggedin users

--- src/app/auth/data/auth-res.data.ts
+++ src/app/auth/data/auth-res.data.ts
@@ -3,6 +3,7 @@ export interface AuthResponse {
     idToken: string;
     email: string;
     refreshToken: string;
+    expiresIn: string;
     localId: string;
     registered?: boolean;
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,26 +1,40 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthResponse } from '../data/auth-res.data';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { User } from '../data/user.data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthServiceComponent {
   private readonly strCtrl = 'AIzaSyALBep5-I2zk3rgeB2lVTR54MkFoZuYBhU';
+  public user = new Subject<User>();
 
   constructor(private http: HttpClient) {}
 
   public signUp(email: string, password: string) {
-    return this.http.post<AuthResponse>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.strCtrl}`,
-      {
-        email,
-        password,
-        refreshToken: true,
-      }
-    ).pipe(catchError(this.erorHanlder));
+    return this.http
+      .post<AuthResponse>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.strCtrl}`,
+        {
+          email,
+          password,
+          refreshToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.erorHanlder),
+        tap((resData) => {
+          this.handleAuth(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   public login(email: string, password: string) {
@@ -33,7 +47,29 @@ export class AuthServiceComponent {
           refreshToken: true,
         }
       )
-      .pipe(catchError(this.erorHanlder));
+      .pipe(
+        catchError(this.erorHanlder),
+        tap((resData) => {
+          this.handleAuth(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuth(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private erorHanlder(errorResponse: HttpErrorResponse) {
@@ -44,30 +80,32 @@ export class AuthServiceComponent {
 
     switch (errorResponse.error.error.message) {
       case 'EMAIL_NOT_FOUND':
-      errMsg = 'There is no user record corresponding to this identifier. The user may have been deleted.';
-      break;
+        errMsg =
+          'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
 
       case 'EMAIL_EXISTS':
-      errMsg = 'There is a user regisreted with the same e-mail address!';
-      break;
+        errMsg = 'There is a user regisreted with the same e-mail address!';
+        break;
 
       case 'OPERATION_NOT_ALLOWED':
-      errMsg = 'Password sign-in is disabled for this project!';
-      break;
+        errMsg = 'Password sign-in is disabled for this project!';
+        break;
 
       case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-      errMsg = 'We have blocked all requests from this device due to unusual activity. Try again later!';
-      break;
+        errMsg =
+          'We have blocked all requests from this device due to unusual activity. Try again later!';
+        break;
 
       case 'INVALID_PASSWORD':
-      errMsg = 'Provided password is incorrect!';
-      break;  
+        errMsg = 'Provided password is incorrect!';
+        break;
 
       case 'USER_DISABLED':
-      errMsg = 'The user account has been disabled by an administrator';
-      break;
+        errMsg = 'The user account has been disabled by an administrator';
+        break;
     }
-    
+
     return throwError(errMsg);
   }
 }
